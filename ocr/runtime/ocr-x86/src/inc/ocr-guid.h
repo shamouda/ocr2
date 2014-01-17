bca95975f58860d50fd05457d57f02cb8f8eb951
Romain <romain.e.cledat@intel.com>
2014-01-17 03:49:30 -0800

    OCR: Got the first hello world example to run!
        - Added begin to complement start (just like stop/finish).
          The ordering is now begin followed by start, then stop and finish
        - Fixed the range tracker
        - Fixed a whole bunch of bugs to get the initial code working.
          It works for the hello world with only one thread for now. There
          is also a very ugly hack for printf
        - Re-enabled debug compilation and added -Wall
    
    Change-Id: I6c96583b28f9e3175509e9b1fc1522ea2c9e1775

--- ocr/runtime/ocr-x86/src/inc/ocr-guid.h
+++ ocr/runtime/ocr-x86/src/inc/ocr-guid.h
@@ -61,6 +61,8 @@ typedef struct _ocrGuidProviderFcts_t {
      */
     void (*destruct)(struct _ocrGuidProvider_t* self);
 
+    void (*begin)(struct _ocrGuidProvider_t *self, struct _ocrPolicyDomain_t *pd);
+    
     /**
      * @brief "Starts" the GUID provider
      *
