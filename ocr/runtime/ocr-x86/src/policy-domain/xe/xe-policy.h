7b974126740d85e3c52aca817dcb3dafcf781e7b
Sanjay Chatterjee <sanjay.chatterjee@intel.com>
2014-04-03 17:24:41 -0700

    Increased task pool to enable examples/cholesky to finish
    Change-Id: Ie762b1137cd66515735c8dc73e4575fa4da27662

--- ocr/runtime/ocr-x86/src/policy-domain/xe/xe-policy.h
+++ ocr/runtime/ocr-x86/src/policy-domain/xe/xe-policy.h
@@ -13,7 +13,7 @@
 #include "ocr-policy-domain.h"
 #include "utils/ocr-utils.h"
 
-#define MAX_XE_TASK 16
+#define MAX_XE_TASK 1024
 
 /******************************************************/
 /* OCR-XE POLICY DOMAIN                               */
