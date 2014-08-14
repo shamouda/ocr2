7486208ef1e5316575afe0fad456947e6e444c71
sagnak <sagnak@rice.edu>
2014-08-14 02:58:07 -0700

    update make&script for more MKL, add single MKL call version

--- ocr/runtime/ocr-x86/examples/cholesky/compile.sh
+++ ocr/runtime/ocr-x86/examples/cholesky/compile.sh
@@ -7,14 +7,27 @@ for ocrbuild in .tbb "" ; do
         fi
         for model in .ocr .serialized; do
             for allocator in "" .tbb; do
-                for dimension in "" .1D .1D.noalloc; do
-                    for repeat in "" .repeat; do
+                for repeat in "" .repeat; do
+                    for dimension in "" .1D .1D.noalloc; do
                         if [[ ".ocr" == "$model" ]]; then 
                             for link in "" .static; do
                                 OCR_INSTALL=$HOME/ocr.builds/ocr$arch.icc.env$ocrbuild LD_LIBRARY_PATH=$OCR_INSTALL/lib:$LD_LIBRARY_PATH CC=icc make compile$compile_arch$model$allocator$link$dimension$repeat.
                             done
                         else
                             CC=icc make compile$compile_arch$model$allocator$dimension$repeat.
+                        fi
+                        if [[ ".1D" == "$dimension" ]]; then 
+                            for mklmodel in .seq .multi; do
+                                for link in "" .static; do
+                                    if [[ ".ocr" == "$model" ]]; then 
+                                        OCR_INSTALL=$HOME/ocr.builds/ocr$arch.icc.env$ocrbuild LD_LIBRARY_PATH=$OCR_INSTALL/lib:$LD_LIBRARY_PATH CC=icc make compile$compile_arch$model$allocator$link$mklmodel.mkl$dimension$repeat.
+                                    else
+                                        for kernel in "" .dpotrf; do
+                                            CC=icc make compile$compile_arch$model$allocator$link$mklmodel.mkl$dimension$repeat$kernel.
+                                        done
+                                    fi
+                                done
+                            done
                         fi
                     done
                 done
