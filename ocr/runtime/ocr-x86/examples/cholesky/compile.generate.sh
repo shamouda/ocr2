d7a913a7ebdfe9c02bb2170796f9668e025e6ece
sagnak <sagnak@rice.edu>
2015-03-24 19:09:57 -0500

    update cholesky makefiles and compile shell scripts

--- ocr/runtime/ocr-x86/examples/cholesky/compile.generate.sh
+++ ocr/runtime/ocr-x86/examples/cholesky/compile.generate.sh
@@ -1,3 +1,5 @@
+MAKEFILE=Makefile.generate
+INSTRUMENT=
 for ocrbuild in .tbb "" ; do
     for arch in .x86 .mic; do
         if [[ ".x86" == "$arch" ]]; then
@@ -12,19 +14,19 @@ for ocrbuild in .tbb "" ; do
                         for link in "" .static; do
                             TARGETNAME=compile$compile_arch$model$allocator$link$dimension$repeat.
                             if [[ ".ocr" == "$model" ]]; then 
-                                OCR_INSTALL=$HOME/ocr.builds/ocr$arch.icc.env$ocrbuild LD_LIBRARY_PATH=$OCR_INSTALL/lib:$LD_LIBRARY_PATH CC=icc make -f Makefile.generate $TARGETNAME
+                                INSTRUMENT=$INSTRUMENT OCR_INSTALL=$HOME/ocr.builds/ocr$arch.icc.env$ocrbuild LD_LIBRARY_PATH=$OCR_INSTALL/lib:$LD_LIBRARY_PATH CC=icc make -f $MAKEFILE $TARGETNAME
                             else
-                                CC=icc make -f Makefile.generate $TARGETNAME
+                                INSTRUMENT=$INSTRUMENT CC=icc make -f $MAKEFILE $TARGETNAME
                             fi
                             done
                         if [[ ".1D" == "$dimension" ]]; then 
                             for mklmodel in .seq .multi; do
                                 for link in "" .static; do
                                     if [[ ".ocr" == "$model" ]]; then 
-                                        OCR_INSTALL=$HOME/ocr.builds/ocr$arch.icc.env$ocrbuild LD_LIBRARY_PATH=$OCR_INSTALL/lib:$LD_LIBRARY_PATH CC=icc make -f Makefile.generate compile$compile_arch$model$allocator$link$mklmodel.mkl$dimension$repeat.
+                                        INSTRUMENT=$INSTRUMENT OCR_INSTALL=$HOME/ocr.builds/ocr$arch.icc.env$ocrbuild LD_LIBRARY_PATH=$OCR_INSTALL/lib:$LD_LIBRARY_PATH CC=icc make -f $MAKEFILE compile$compile_arch$model$allocator$link$mklmodel.mkl$dimension$repeat.
                                     else
                                         for kernel in "" .dpotrf; do
-                                            CC=icc make -f Makefile.generate compile$compile_arch$model$allocator$link$mklmodel.mkl$dimension$repeat$kernel.
+                                            INSTRUMENT=$INSTRUMENT CC=icc make -f $MAKEFILE compile$compile_arch$model$allocator$link$mklmodel.mkl$dimension$repeat$kernel.
                                         done
                                     fi
                                 done
