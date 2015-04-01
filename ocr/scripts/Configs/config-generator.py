8a381f7a50bb5b48a7ad66625110fad531e77b5f
Bala S <bala.seshasayee@intel.com>
2015-04-01 13:51:52 -0700

    OCR config file cleanup
    A few more fixes
    - Cleaner (limited) options with suitable defaults
    - Cleanup binding
    - Make lockable DBs the default
    - Support for X86-pthread-mpi
    - Cleanup deprecated CFG sections under machine-configs/
    - Add --remove-destination option to overwrite existing cfg file
    
    See #496
    
    Change-Id: Ib5080665b20fa6b9c4e4d4113f351e097316fc8c

--- ocr/scripts/Configs/config-generator.py
+++ ocr/scripts/Configs/config-generator.py
@@ -12,39 +12,46 @@ import sys
 # 2. Read FSim machine config to spit out OCR-FSim configs
 
 parser = argparse.ArgumentParser(description='Generate an OCR config file.')
-parser.add_argument('--guid', dest='guid', default='PTR',
+parser.add_argument('--guid', dest='guid', default='PTR', choices=['PTR', 'COUNTED_MAP'],
                    help='guid type to use (default: PTR)')
-parser.add_argument('--platform', dest='platform', default='X86',
+parser.add_argument('--platform', dest='platform', default='X86', choices=['X86', 'FSIM'],
                    help='platform type to use (default: X86)')
-parser.add_argument('--target', dest='target', default='X86',
+parser.add_argument('--target', dest='target', default='X86', choices=['X86', 'FSIM', 'MPI'],
                    help='target type to use (default: X86)')
-parser.add_argument('--threads', dest='threads', default='4',
+parser.add_argument('--threads', dest='threads', type=int, default=4,
                    help='number of threads available to OCR (default: 4)')
-parser.add_argument('--binding', dest='binding', default='0',
-                   help='thread binding to use (default: no binding)')
+parser.add_argument('--binding', dest='binding', default='none', choices=['none', 'seq', 'spread'],
+                   help='perform thread binding (default: no binding)')
 parser.add_argument('--alloc', dest='alloc', default='32',
                    help='size (in MB) of memory available for app use (default: 32)')
-parser.add_argument('--alloctype', dest='alloctype', default='mallocproxy',
+parser.add_argument('--alloctype', dest='alloctype', default='mallocproxy', choices=['mallocproxy', 'tlsf', 'simple'],
                    help='type of allocator to use (default: mallocproxy)')
+parser.add_argument('--dbtype', dest='dbtype', default='Lockable', choices=['Lockable', 'Regular'],
+                   help='type of datablocks to use (default: Lockable)')
 parser.add_argument('--output', dest='output', default='default.cfg',
                    help='config output filename (default: default.cfg)')
+parser.add_argument('--remove-destination', dest='rmdest', action='store_true',
+                   help='remove output file if exists (default: no)')
+
 
 args = parser.parse_args()
 guid = args.guid
 platform = args.platform
 target = args.target
-threads = int(args.threads)
+threads = args.threads
 binding = args.binding
 alloc = args.alloc
 alloctype = args.alloctype
+dbtype = args.dbtype
 outputfilename = args.output
+rmdest = args.rmdest
 
 def GenerateGuid(output, guid):
     output.write("[GuidType0]\n\tname\t=\t%s\n\n" % (guid))
     output.write("[GuidInst0]\n\tid\t=\t%d\n\ttype\t=\t%s\n" % (0, guid))
     output.write("\n#======================================================\n")
 
-def GeneratePd(output, pdtype, threads):
+def GeneratePd(output, pdtype, dbtype, threads):
     output.write("[PolicyDomainType0]\n\tname\t=\t%s\n\n" % (pdtype))
     output.write("[PolicyDomainInst0]\n")
     output.write("\tid\t\t\t=\t0\n")
@@ -52,20 +59,23 @@ def GeneratePd(output, pdtype, threads):
     output.write("\tworker\t\t\t=\t0-%d\n" % (threads-1))
     output.write("\tscheduler\t\t=\t0\n")
     output.write("\tallocator\t\t=\t0\n")
-    output.write("\tcommapi\t\t\t=\t0\n")
+    if pdtype == 'HCDist':
+        output.write("\tcommapi\t\t\t=\t0-%d\n" % (threads-1))
+    else:
+        output.write("\tcommapi\t\t\t=\t0\n")
     output.write("\tguid\t\t\t=\t0\n")
     output.write("\tparent\t\t\t=\t0\n")
     output.write("\tlocation\t\t=\t0\n")
     pdtype = "HC"
     output.write("\ttaskfactory\t\t=\t%s\n" % (pdtype))
     output.write("\ttasktemplatefactory\t=\t%s\n" % (pdtype))
-    output.write("\tdatablockfactory\t=\tRegular\n")
+    output.write("\tdatablockfactory\t=\t%s\n" % (dbtype))
     output.write("\teventfactory\t\t=\t%s\n\n" % (pdtype))
 
-def GenerateCommon(output, pdtype):
+def GenerateCommon(output, pdtype, dbtype):
     output.write("[TaskType0]\n\tname=\t%s\n\n" % (pdtype))
     output.write("[TaskTemplateType0]\n\tname=\t%s\n\n" % (pdtype))
-    output.write("[DataBlockType0]\n\tname=\tRegular\n\n")
+    output.write("[DataBlockType0]\n\tname=\t%s\n\n" % (dbtype))
     output.write("[EventType0]\n\tname=\t%s\n\n" % (pdtype))
     output.write("\n#======================================================\n")
 
@@ -91,28 +101,75 @@ def GenerateMem(output, size, count, alloctype):
     output.write("\tmemtarget\t=\t0\n")
     output.write("\n#======================================================\n")
 
-def GenerateComm(output, comms):
-    output.write("[CommApiType0]\n\tname\t=\t%s\n" % ("Handleless"))
-    output.write("[CommApiInst0]\n")
-    output.write("\tid\t=\t0\n")
-    output.write("\ttype\t=\t%s\n" % ("Handleless"))
-    output.write("\tcommplatform\t=\t0\n")
+def GenerateComm(output, comms, pdtype, threads):
     output.write("\n#======================================================\n")
-    output.write("[CommPlatformType0]\n\tname\t=\t%s\n" % ("None"))
-    output.write("[CommPlatformInst0]\n")
-    output.write("\tid\t=\t0\n")
-    output.write("\ttype\t=\t%s\n" % ("None"))
+    if pdtype == 'HCDist':
+        output.write("[CommApiType0]\n\tname\t=\t%s\n" % ("Simple"))
+        output.write("[CommApiInst0]\n")
+        output.write("\tid\t=\t0\n")
+        output.write("\ttype\t=\t%s\n" % ("Simple"))
+        output.write("\tcommplatform\t=\t0\n")
+        output.write("[CommApiType1]\n\tname\t=\t%s\n" % ("Delegate"))
+        output.write("[CommApiInst1]\n")
+        output.write("\tid\t=\t1-%d\n" % (threads-1))
+        output.write("\ttype\t=\t%s\n" % ("Delegate"))
+        output.write("\tcommplatform\t=\t1-%d\n" % (threads-1))
+        output.write("\n#======================================================\n")
+        output.write("[CommPlatformType0]\n\tname\t=\t%s\n" % ("None"))
+        output.write("[CommPlatformInst0]\n")
+        output.write("\tid\t=\t1-%d\n" % (threads-1))
+        output.write("\ttype\t=\t%s\n" % ("None"))
+        output.write("[CommPlatformType1]\n\tname\t=\t%s\n" % ("MPI"))
+        output.write("[CommPlatformInst1]\n")
+        output.write("\tid\t=\t0\n")
+        output.write("\ttype\t=\t%s\n" % ("MPI"))
+    else:
+        output.write("[CommPlatformType0]\n\tname\t=\t%s\n" % ("None"))
+        output.write("[CommPlatformInst0]\n")
+        output.write("\tid\t=\t0\n")
+        output.write("\ttype\t=\t%s\n" % ("None"))
+        output.write("\n#======================================================\n")
+        output.write("[CommApiType0]\n\tname\t=\t%s\n" % ("Handleless"))
+        output.write("[CommApiInst0]\n")
+        output.write("\tid\t=\t0\n")
+        output.write("\ttype\t=\t%s\n" % ("Handleless"))
+        output.write("\tcommplatform\t=\t0\n")
+        output.write("\n#======================================================\n")
+        output.write("[CommPlatformType0]\n\tname\t=\t%s\n" % ("None"))
+        output.write("[CommPlatformInst0]\n")
+        output.write("\tid\t=\t0\n")
+        output.write("\ttype\t=\t%s\n" % ("None"))
     output.write("\n#======================================================\n")
 
-def GenerateComp(output, threads, binding):
+def GenerateComp(output, pdtype, threads, binding, schedtype):
     output.write("[CompPlatformType0]\n\tname\t=\t%s\n" % ("pthread"))
     output.write("\tstacksize\t=\t0\n")
     output.write("[CompPlatformInst0]\n")
     output.write("\tid\t=\t0-%d\n" % (threads-1))
     output.write("\ttype\t=\t%s\n" % ("pthread"))
     output.write("\tstacksize\t=\t0\n")
-    if binding != 0:
-        output.write("\tbinding\t=\t%s\n" % (binding))
+    if binding != 'none':
+        output.write("\tbinding\t=\t")
+        if binding == 'seq':
+            for i in range(0, threads-1):
+                output.write("%d," % i)
+            output.write("%d\n" % (threads-1))
+        else: # binding == spread
+            count = 0
+            for i in range(0, threads, 2):
+                output.write("%d" % i)
+                count = count+1
+                if count != threads:
+                    output.write(",")
+                else:
+                    output.write("\n")
+            for i in range(1, threads, 2):
+                output.write("%d" % i)
+                count = count+1
+                if count != threads:
+                    output.write(",")
+                else:
+                    output.write("\n")
     output.write("\n#======================================================\n")
     output.write("[CompTargetType0]\n\tname\t=\t%s\n" % ("PASSTHROUGH"))
     output.write("[CompTargetInst0]\n")
@@ -120,70 +177,96 @@ def GenerateComp(output, threads, binding):
     output.write("\ttype\t=\t%s\n" % ("PASSTHROUGH"))
     output.write("\tcompplatform\t=\t0-%d\n" % (threads-1))
     output.write("\n#======================================================\n")
-    output.write("[WorkerType0]\n\tname\t=\t%s\n" % ("HC"))
+    masterWorkerType = "HC_COMM" if (pdtype == 'HCDist') else "HC"
+    output.write("[WorkerType0]\n\tname\t=\t%s\n" % (masterWorkerType))
     output.write("[WorkerInst0]\n")
     output.write("\tid\t=\t0\n")
-    output.write("\ttype\t=\tHC\n")
+    output.write("\ttype\t=\t%s\n" % (masterWorkerType))
     output.write("\tworkertype\t=\tmaster\n")
     output.write("\tcomptarget\t=\t0\n")
-    output.write("[WorkerInst1]\n")
-    output.write("\tid\t=\t1-%d\n" % (threads-1))
-    output.write("\ttype\t=\tHC\n")
-    output.write("\tworkertype\t=\tslave\n")
-    output.write("\tcomptarget\t=\t1-%d\n" % (threads-1))
+    if threads > 1:
+        if (pdtype == 'HCDist'): # Need a second type for distributed
+            output.write("[WorkerType1]\n\tname\t=\tHC\n")
+        output.write("[WorkerInst1]\n")
+        output.write("\tid\t=\t1-%d\n" % (threads-1))
+        output.write("\ttype\t=\tHC\n")
+        output.write("\tworkertype\t=\tslave\n")
+        output.write("\tcomptarget\t=\t1-%d\n" % (threads-1))
     output.write("\n#======================================================\n")
     output.write("[WorkPileType0]\n\tname\t=\t%s\n" % ("HC"))
     output.write("[WorkPileInst0]\n")
     output.write("\tid\t=\t0-%d\n" % (threads-1))
     output.write("\ttype\t=\tHC\n")
     output.write("\n#======================================================\n")
-    output.write("[SchedulerObjectType0]\n")
-    output.write("\tname\t=\t%s\n" % ("WST"))
-    output.write("\tkind\t=\t%s\n" % ("root"))
-    output.write("[SchedulerObjectInst0]\n")
-    output.write("\tid\t=\t0\n")
-    output.write("\ttype\t=\t%s\n" % ("WST"))
-    output.write("[SchedulerObjectType1]\n")
-    output.write("\tname\t=\t%s\n" % ("DEQ"))
-    output.write("[SchedulerObjectInst1]\n")
-    output.write("\tid\t=\t1\n")
-    output.write("\ttype\t=\t%s\n" % ("DEQ"))
-    output.write("\n#======================================================\n")
-    output.write("[SchedulerHeuristicType0]\n\tname\t=\t%s\n" % ("HC"))
-    output.write("[SchedulerHeuristicInst0]\n")
-    output.write("\tid\t=\t0\n")
-    output.write("\ttype\t=\t%s\n" % ("HC"))
-    output.write("\n#======================================================\n")
-    output.write("[SchedulerType0]\n\tname\t=\t%s\n" % ("HC"))
+    if (pdtype == 'HCDist'):
+        output.write("[SchedulerObjectType0]\n")
+        output.write("\tname\t=\tNULL\n")
+        output.write("[SchedulerObjectInst0]\n")
+        output.write("\tid\t=\t0\n")
+        output.write("\ttype\t=\tNULL\n")
+        output.write("\n#======================================================\n")
+        output.write("[SchedulerHeuristicType0]\n\tname\t=\tNULL\n")
+        output.write("[SchedulerHeuristicInst0]\n")
+        output.write("\tid\t=\t0\n")
+        output.write("\ttype\t=\tNULL\n")
+        output.write("\n#======================================================\n")
+    else:
+        output.write("[SchedulerObjectType0]\n")
+        output.write("\tname\t=\t%s\n" % ("WST"))
+        output.write("\tkind\t=\t%s\n" % ("root"))
+        output.write("[SchedulerObjectInst0]\n")
+        output.write("\tid\t=\t0\n")
+        output.write("\ttype\t=\t%s\n" % ("WST"))
+        output.write("[SchedulerObjectType1]\n")
+        output.write("\tname\t=\t%s\n" % ("DEQ"))
+        output.write("[SchedulerObjectInst1]\n")
+        output.write("\tid\t=\t1\n")
+        output.write("\ttype\t=\t%s\n" % ("DEQ"))
+        output.write("\n#======================================================\n")
+        output.write("[SchedulerHeuristicType0]\n\tname\t=\t%s\n" % ("HC"))
+        output.write("[SchedulerHeuristicInst0]\n")
+        output.write("\tid\t=\t0\n")
+        output.write("\ttype\t=\t%s\n" % ("HC"))
+        output.write("\n#======================================================\n")
+    output.write("[SchedulerType0]\n\tname\t=\t%s\n" % (schedtype))
     output.write("[SchedulerInst0]\n")
     output.write("\tid\t=\t0\n")
-    output.write("\ttype\t=\tHC\n")
+    output.write("\ttype\t=\t%s\n" % (schedtype))
     output.write("\tworkpile\t=\t0-%d\n" % (threads-1))
     output.write("\tworkeridfirst\t=\t0\n")
     output.write("\tschedulerObject\t=\t0\n")
     output.write("\tschedulerHeuristic\t=\t0\n")
     output.write("\n#======================================================\n")
 
-def GenerateConfig(filehandle, guid, platform, target, threads, binding, alloc, alloctype):
+def GenerateConfig(filehandle, guid, platform, target, threads, binding, alloc, alloctype, dbtype):
     filehandle.write("# Generated by python script, modify as you see fit\n\n")
     GenerateGuid(filehandle, guid)
-    if binding != '0':
-        bound_threads = binding.count(',')+1
-        if bound_threads != threads:
-            print 'Error: ', threads, ' threads given, however binding is for ', bound_threads
-            raise
     if target=='X86':
-        GeneratePd(filehandle, "HC", threads)
-        GenerateCommon(filehandle, "HC")
+        GeneratePd(filehandle, "HC", dbtype, threads)
+        GenerateCommon(filehandle, "HC", dbtype)
         GenerateMem(filehandle, alloc, 1, alloctype)
-        GenerateComm(filehandle, "null")
-        GenerateComp(filehandle, threads, binding)
+        GenerateComm(filehandle, "null", "HC", threads)
+        GenerateComp(filehandle, "HC", threads, binding, "HC")
     elif target=='FSIM':
-        GeneratePd(filehandle, "CE", 1)
-        GeneratePd(filehandle, "XE", threads)
-        GenerateCommon(filehandle, "HC")
+        GeneratePd(filehandle, "CE", dbtype, 1)
+        GeneratePd(filehandle, "XE", dbtype, threads)
+        GenerateCommon(filehandle, "HC", dbtype)
         GenerateMem(filehandle, alloc, 1, alloctype)
-
+    elif target=='MPI':
+        # catch default value errors for distributed
+        if dbtype != 'Lockable':
+            print 'MPI target only supports Lockable datablocks'
+            raise
+        if guid != 'COUNTED_MAP':
+            print 'MPI target only supports counted-map guid provider'
+            raise
+        pdtype="HCDist"
+        GeneratePd(filehandle, pdtype, dbtype, threads)
+        #Intentionally use "HC" here
+        GenerateCommon(filehandle, "HC", dbtype)
+        GenerateMem(filehandle, alloc, 1, alloctype)
+        GenerateComm(filehandle, "null", pdtype, threads)
+        GenerateComp(filehandle, pdtype, threads, binding, "HC_COMM_DELEGATE")
 
 def GetString(prompt, default):
     prompt = "%s [%s] " % (prompt, default)
@@ -210,12 +293,12 @@ if platform=='X86' or platform=='FSIM':
 
     alloc = int(alloc)*1048576
 
-    if os.path.isfile(outputfilename):
+    if os.path.isfile(outputfilename) and (not rmdest):
         print 'Output file ', outputfilename, ' already exists'
         sys.exit(0)
     else:
         with open(outputfilename, "w") as filehandle:
-            GenerateConfig(filehandle, guid, platform, target, threads, binding, alloc, alloctype)
+            GenerateConfig(filehandle, guid, platform, target, threads, binding, alloc, alloctype, dbtype)
 else:
     print 'Platform ', platform, ' and target ', target, ' unsupported'
     raise
