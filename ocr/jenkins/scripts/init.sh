d7d1885fdef3127e7b6ea411957f0c329e6fbe55
Romain <romain.e.cledat@intel.com>
2015-09-04 18:54:35 -0700

    Jenkins: Fixes to the init script to run faster
    The repository has grown so large with so many files that
    jobs were timing out. This addresses the issue by excluding some
    unecessary libraries in the initialization phase
    
    Change-Id: I5c9eeadb1c7f4c47db1c935e00d4549074c523af

--- ocr/jenkins/scripts/init.sh
+++ ocr/jenkins/scripts/init.sh
@@ -24,7 +24,8 @@ cp -r ${JJOB_PRIVATE_HOME}/xstack/ocr/scripts/* ${JJOB_SHARED_HOME}/xstack/ocr/s
 cp -r ${JJOB_PRIVATE_HOME}/xstack/ocr/examples/* ${JJOB_SHARED_HOME}/xstack/ocr/examples/
 rsync -aq -r ${JJOB_PRIVATE_HOME}/xstack/apps/ ${JJOB_SHARED_HOME}/xstack/apps/ --exclude libs
 mkdir -p ${JJOB_SHARED_HOME}/xstack/apps/libs
-rsync -aq -r ${JJOB_PRIVATE_HOME}/xstack/apps/libs/src ${JJOB_SHARED_HOME}/xstack/apps/libs/src --exclude trilinos
+rsync -aq -r ${JJOB_PRIVATE_HOME}/xstack/apps/libs/src ${JJOB_SHARED_HOME}/xstack/apps/libs --exclude trilinos --exclude libcxx --exclude libswtest --exclude libunwind --exclude libcxxabi --exclude ocrscaffold --exclude "ocr-glibc"
+#find ${JJOB_PRIVATE_HOME}/xstack/apps/libs/src -maxdepth 1 -type d -not -name "trilinos" -print0 | xargs -0 cp -r -t ${JJOB_SHARED_HOME}/xstack/apps/libs/src
 cp -rT ${JJOB_PRIVATE_HOME}/xstack/hll/cnc ${JJOB_SHARED_HOME}/xstack/hll/cnc
 
 if [ "x$TG" == "xtg" ]; then
