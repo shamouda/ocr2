0a6e422a1f45f6cc706c18298ca327001441c2d7
vcave <vincent.cave@intel.com>
2015-10-22 15:32:55 -0700

    Jenkins micro-benchmark digest report tentative
    Change-Id: Icc08bb5325ffa0eab186a418cbdb05abe09c13ce

--- ocr/tests/performance-tests/scripts/drivers/foobar.sh
+++ ocr/tests/performance-tests/scripts/drivers/foobar.sh
@@ -59,3 +59,71 @@ SCRIPT_ROOT=./scripts ./scripts/perfDriver.sh event2StickyFanOutStickySatisfy
 SCRIPT_ROOT=./scripts ./scripts/perfDriver.sh event2LatchFanOutLatchSatisfy
 export CUSTOM_BOUNDS="NB_ITERS=3000000 FAN_OUT=4" # 1.3s
 SCRIPT_ROOT=./scripts ./scripts/perfDriver.sh event2OnceFanOutOnceSatisfy
+
+#
+# Pretty printing results into a digest report
+#
+
+function extractThroughput() {
+    local file=$1
+    local  __resultvar=$2
+    res=`more ${file} | grep "^1" | sed -e "s/  / /g" | cut -d' ' -f2-2`
+    eval $__resultvar="'$res'"
+}
+
+# Returns individual operation duration in nanoseconds
+function getTimePerOpFromThroughput() {
+    local file=$1
+    local  __resultvar=$2
+    tput=
+    extractThroughput $file tput
+    scale=9
+    res=`echo "scale=$scale; (10^9 / $tput)" | bc`
+    eval $__resultvar="'$res'"
+}
+
+function printTimePerOpNano() {
+    local file=$1
+    local text=$2
+    local outfile=$3
+    opDuration=
+    getTimePerOpFromThroughput $file opDuration
+    echo "$text $opDuration (ns)" >> ${outfile}
+}
+
+#
+# Nice printing
+#
+
+outfile="$PWD/digest-report"
+
+echo "Average OCR operations cost for x86 with single OCR worker" > ${outfile}
+echo "" >> ${outfile}
+
+printTimePerOpNano report-edtCreateLatchSync ${outfile} '* An EDT creation takes:'
+printTimePerOpNano report-edtCreateFinishSync ${outfile} '* An EDT creation inside a finish EDT takes:'
+printTimePerOpNano report-edtTemplate0Create ${outfile} '* An EDT template creation takes:'
+printTimePerOpNano report-edtTemplate0Destroy ${outfile} '* An EDT template destruction takes:'
+
+printTimePerOpNano report-event0StickyCreate ${outfile} '* A sticky event creation takes:'
+printTimePerOpNano report-event0OnceCreate ${outfile} '* A once event creation takes:'
+printTimePerOpNano report-event0LatchCreate ${outfile} '* A latch event creation takes:'
+
+printTimePerOpNano report-event0StickyDestroy ${outfile} '* A sticky event destruction takes:'
+printTimePerOpNano report-event0OnceDestroy ${outfile} '* A once event destruction takes:'
+printTimePerOpNano report-event0LatchDestroy ${outfile} '* A latch event destruction takes:'
+
+FAN_OUT=4
+printTimePerOpNano report-event1StickyFanOutEdtAddDep ${outfile} "* Add dependences between a source sticky event to ${FAN_OUT} EDT's slots takes:"
+printTimePerOpNano report-event1OnceFanOutEdtAddDep ${outfile} " *Add dependences between a source once event to ${FAN_OUT} EDT's slots takes:"
+
+printTimePerOpNano report-event1StickyFanOutEdtSatisfy ${outfile} "* Satisfy a sticky event that has ${FAN_OUT} EDT as dependences takes:"
+printTimePerOpNano report-event1OnceFanOutEdtSatisfy ${outfile} "* Satisfy a once event that has ${FAN_OUT} EDT as dependences takes:"
+
+printTimePerOpNano report-event2StickyFanOutStickyAddDep ${outfile} "* Add dependences between a source sticky event to ${FAN_OUT} sticky events takes:"
+printTimePerOpNano report-event2OnceFanOutOnceAddDep ${outfile} "* Add dependences between a source once event to ${FAN_OUT} once events takes:"
+printTimePerOpNano report-event2LatchFanOutLatchAddDep ${outfile} "* Add dependences between a source latch event to ${FAN_OUT} latch events takes:"
+
+printTimePerOpNano report-event2StickyFanOutStickySatisfy ${outfile} "* Satisfy a sticky event that has ${FAN_OUT} sticky as dependences takes:"
+printTimePerOpNano report-event2LatchFanOutLatchSatisfy ${outfile} "* Satisfy a latch event that has ${FAN_OUT} latch as dependences takes:"
+printTimePerOpNano report-event2OnceFanOutOnceSatisfy ${outfile} "* Satisfy a once event that has ${FAN_OUT} once as dependences takes:"
